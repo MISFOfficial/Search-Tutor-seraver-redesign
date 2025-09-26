919324abccf357ff4847877bccd0c81af938d46c
Muksitul Islam <muksitul15-3962@diu.edu.bd>
2025-09-26 10:58:37 +0600

    none git commit -m none

--- index.js
+++ index.js
@@ -9,12 +9,12 @@ const port = process.env.PORT || 4000;
 app.use(cors());
 app.use(express.json());
 
+
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 //o current data base api
 // const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mjmwf3r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
-const uri=process.env.URI
-
+const uri = process.env.URI
 
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
